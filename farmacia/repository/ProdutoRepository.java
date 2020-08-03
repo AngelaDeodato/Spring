68fe1f91f74fcd8f1c2ad287d04a53d7e8ce571a
Angela Gabriela <angeladeodato2017@hotmail.com>
2020-08-03 15:42:54 -0300

    Novos exercicios SPRING

---
+++ farmacia/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.farmacia.farmaciaGen.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.farmacia.farmaciaGen.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+
+}
