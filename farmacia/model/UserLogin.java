a743d7378e34e8acebd265a289c16460ca9f9c60
Angela Gabriela <angeladeodato2017@hotmail.com>
2020-08-10 17:29:58 -0300

    Security da aplicação farmaciaGen

---
+++ farmacia/model/UserLogin.java
@@ -0,0 +1,38 @@
+package com.farmacia.farmaciaGen.model;
+
+public class UserLogin {
+	
+	private String nome;
+	private String usuario;
+	private String senha;
+	private String token;
+	
+	
+	//METODOS ESPECIAIS GETTERS E SETTERS
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+}
